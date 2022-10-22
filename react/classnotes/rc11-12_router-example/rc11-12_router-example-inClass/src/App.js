2c0dea8404c408bb17e947fac97d062066777a7f
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-22 12:36:01 +0300

    routes

--- react/classnotes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ react/classnotes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -4,15 +4,20 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+import Paths from "./pages/Paths";
 
 function App() {
   return (
     <>
       <Nav />
-      <Home />
-      <People />
-      <Contact />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/people" element={<People />}></Route>
+        <Route path="/paths" element={<Paths />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
       <Footer />
     </>
   );

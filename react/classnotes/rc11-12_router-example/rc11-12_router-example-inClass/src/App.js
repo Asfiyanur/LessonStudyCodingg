72a85af2d0b183a405868aa379f1d78d1b8a94a6
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-24 21:27:28 +0300

    navigateCompHook

--- react/classnotes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ react/classnotes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -4,7 +4,7 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import People from "./pages/People";
 import NotFound from "./pages/NotFound";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Paths from "./pages/Paths";
 import PersonDetail from "./pages/PersonDetail";
 
@@ -13,12 +13,13 @@ function App() {
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/people/:id" element={<PersonDetail />} />
-        <Route path="/paths" element={<Paths />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route index element={<Home />} />
+        <Route path="people" element={<People />} />
+        <Route path="people/:id" element={<PersonDetail />} />
+        <Route path="paths" element={<Paths />} />
+        <Route path="contact" element={<Contact />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>

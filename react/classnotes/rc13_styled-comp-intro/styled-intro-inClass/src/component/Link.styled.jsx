2d0a33ac670ecb367e12e9d440d09e3ecee0bcaa
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-25 21:39:42 +0300

    styledDone

---
+++ react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/component/Link.styled.jsx
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+const StyledLink = styled.a`
+  font-size: 1rem;
+  text-align: none;
+  &:hover {
+    opacity: 0.5;
+  }
+  ${(props) =>
+    props.secondary &&
+    css`
+      background-color: lightblue;
+      color: crimson;
+      padding: 0.3rem;
+    `}
+`;
+
+export default StyledLink;

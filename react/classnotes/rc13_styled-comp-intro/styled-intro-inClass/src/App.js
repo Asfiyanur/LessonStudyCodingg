e7cb40cc231cc064f98468f9d28ff4dc94b7f47e
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-25 20:56:37 +0300

    TomatoButton

--- react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,8 +1,15 @@
+import { Button, TomatoButton } from "./component/Button.styled";
+import Container from "./component/Container.styled";
+
 const App = () => {
   return (
-    <>
+    <Container>
       <h1>Styled Components</h1>
-    </>
+      <Button primary>Click</Button>
+      <Button>Clear</Button>
+      <TomatoButton>Send</TomatoButton>
+      <TomatoButton primary>Submit</TomatoButton>
+    </Container>
   );
 };
 
---
+++ react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/component/Button.styled.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  /* background-color: hotpink; */
+  background-color: ${(props) => (props.primary ? "whihte" : "purple")};
+
+  /* color: white; */
+  color: ${({ primary }) => (primary ? "purple" : "white")};
+
+  cursor: pointer;
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  border-color: hotpink;
+  &:hover {
+    transform: scale(0.97);
+  }
+`;
+
+export const TomatoButton = styled(Button)`
+  background-color: ${(props) => (props.primary ? "white" : "tomato")};
+
+  color: ${({ primary }) => (primary ? "tomato" : "white")};
+`;

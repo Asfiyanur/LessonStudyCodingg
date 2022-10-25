9cf35797bd9a5f2ce64fe8bf104ec3999cd3e899
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-25 22:56:21 +0300

    Clarusway

--- react/classnotes/rc13_styled-comps-example/styled-example-inClass/src/App.js
+++ react/classnotes/rc13_styled-comps-example/styled-example-inClass/src/App.js
@@ -1,8 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+
+const style = {
+  colors: { header: "#fff", body: "fff", footer: "#8A1C4A" },
+  margin: {},
+  responsive: {},
+};
+
 const App = () => {
   return (
-    <>
-      <h1>APP</h1>
-    </>
+    <ThemeProvider theme={style}>
+      <Header />
+    </ThemeProvider>
   );
 };
 
---
+++ react/classnotes/rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Button from "./styles/Button.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Nav>
+        <Logo src="./images/logo.png" />
+        <div>
+          <Button color="#A62440">Apply Courses</Button>
+          <Button bg="#A62440">Talk to Adviser</Button>
+        </div>
+      </Nav>
+    </StyledHeader>
+  );
+};
+
+export default Header;

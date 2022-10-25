cdd27f2cccb4c6b4530c5f9bd2e5be8307b8ca0f
Asfiyanur <nurasfiya.13@gmail.com>
2022-10-25 21:40:03 +0300

    done

--- react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ react/classnotes/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,15 +1,29 @@
 import { Button, TomatoButton } from "./component/Button.styled";
 import Container from "./component/Container.styled";
+import HeaderText from "./component/Header.styled";
+import StyledLink from "./component/Link.styled";
 
 const App = () => {
   return (
-    <Container>
-      <h1>Styled Components</h1>
-      <Button primary>Click</Button>
-      <Button>Clear</Button>
-      <TomatoButton>Send</TomatoButton>
-      <TomatoButton primary>Submit</TomatoButton>
-    </Container>
+    <>
+      <Container bg="grey">
+        <HeaderText color="hotpink"> STYLED COMPONENT</HeaderText>
+        <Button primary>Click</Button>
+        <Button>Clear</Button>
+        <TomatoButton>Send</TomatoButton>
+        <TomatoButton primary>Submit</TomatoButton>
+      </Container>
+
+      <Container bg="#eee">
+        <StyledLink href="https://www.google.com.tr/?hl=tr" target="_blank">
+          Google
+        </StyledLink>
+
+        <StyledLink href="https://www.google.com.tr/?hl=tr" target="_blank">
+          React
+        </StyledLink>
+      </Container>
+    </>
   );
 };
 

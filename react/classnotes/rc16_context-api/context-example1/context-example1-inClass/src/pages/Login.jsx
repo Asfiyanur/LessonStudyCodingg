0f0e9c6a88938caa1f8c5c379dcff753bd7898fd
Asfiyanur <nurasfiya.13@gmail.com>
2022-11-02 22:19:20 +0300

    login

--- react/classnotes/rc16_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
+++ react/classnotes/rc16_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
@@ -3,12 +3,16 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [user, setUser] = useContext(LoginContext);
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    navigate(-1);
   };
 
   return (

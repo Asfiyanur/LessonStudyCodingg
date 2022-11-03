87ab50552e678c5f16bbd5b2d60e01bcb04de52a
Asfiyanur <nurasfiya.13@gmail.com>
2022-11-03 20:54:11 +0300

    butttoncolor

--- react/classnotes/rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
+++ react/classnotes/rc17_material-intro/material-intro-inClass/src/components/TypoButtons.jsx
@@ -2,6 +2,8 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
 
 const TypoButtons = () => {
   return (
@@ -13,7 +15,7 @@ const TypoButtons = () => {
 
         <Typography
           variant="body1"
-          align="justfiy"
+          align="justify"
           mt={4}
           sx={{
             backgroundColor: "lightgreen",
@@ -37,10 +39,24 @@ const TypoButtons = () => {
       </Container>
 
       <Container>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5rem",
+            width: "50%",
+            margin: "0 auto",
+          }}>
           <Button variant="text">Text</Button>
           <Button variant="contained">Contained</Button>
           <Button variant="outlined">Outlined</Button>
+
+          <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Button variant="contained" color="secondary" endIcon={<SendIcon />}>
+            Send
+          </Button>
         </Box>
       </Container>
     </>

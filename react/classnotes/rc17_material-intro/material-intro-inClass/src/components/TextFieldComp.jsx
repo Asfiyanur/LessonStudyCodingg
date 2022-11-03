9d28fe691a96b0a3078c7115c93a7d8e0b9f617b
Asfiyanur <nurasfiya.13@gmail.com>
2022-11-03 21:33:11 +0300

    button

---
+++ react/classnotes/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+
+const TextFieldComp = () => {
+  const [error, setError] = useState(false);
+
+  return (
+    <div>
+      <Container>
+        <Typography variant="h4" color="error" align="center" marginTop={4}>
+          Text Field
+        </Typography>
+
+        <Box sx={{ mt: 4, textAling: "center" }}>
+          <TextField
+            id="email"
+            label="Email"
+            placeholder="enter your email"
+            error={false}
+            fullWidth
+          />
+          <TextField
+            id="password"
+            label="password"
+            placeholder="enter your password"
+            error={false}
+            fullWidth
+          />
+          <Button variant="contained" color="warning" sx={{ mt: 3 }}>
+            submit
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default TextFieldComp;

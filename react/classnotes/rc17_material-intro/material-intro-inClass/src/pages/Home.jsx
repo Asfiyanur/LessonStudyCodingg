a549db77c6bf4e2f166ebab1254227f4d2b44d69
Asfiyanur <nurasfiya.13@gmail.com>
2022-11-03 23:11:17 +0300

    theme

--- react/classnotes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
+++ react/classnotes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
@@ -2,15 +2,21 @@ import AppBarComp from "../components/AppBarComp";
 import CardGrid from "../components/CardGrid";
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const Home = () => {
+  const theme = createTheme({
+    palette: {
+      primary: { main: "#F17A97", light: "#F3BCC8", dark: "#F2526E" },
+    },
+  });
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <AppBarComp />
       <TypoButtons />
       <TextFieldComp />
       <CardGrid />
-    </>
+    </ThemeProvider>
   );
 };
 

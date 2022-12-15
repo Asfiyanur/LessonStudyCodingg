1a146086d5f711850b3d03c53a1ddf3ffb54b87c
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-15 15:44:30 +0300

    movieCallstart

--- react/studyHomework/movie-app-react-redux starter/src/app/store.jsx
+++ react/studyHomework/movie-app-react-redux starter/src/app/store.jsx
@@ -2,10 +2,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import movieReducer from "../features/movieSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    movie: movieReducer,
   },
 });
 

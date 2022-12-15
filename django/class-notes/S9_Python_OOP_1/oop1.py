38b7d1178c902c3596d5585d800a25155143ed3a
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-15 22:17:57 +0300

    oopint

--- django/class-notes/S9_Python_OOP_1/oop1.py
+++ django/class-notes/S9_Python_OOP_1/oop1.py
@@ -9,15 +9,64 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 # print_types(test)
 
-class Person:
-    name ='asf'
-    age=28
+# class Person:
+#     name ='asf'
+#     age=28
     
-person1=Person()
-person2=Person()
+# person1=Person()
+# person2=Person()
 
-print(person1.name)
-print(person2.age)
+# print(person1.name)
+# print(person2.age)
 
-Person.job ='developer'
-print(person1.job)
+# Person.job ='developer'
+# print(person1.job)
+
+# class Person :
+    
+#     company ='clarusway'
+    
+#     def set_details(self,name,age):
+#         self.name=name
+#         self.age=age
+        
+#     def get_details(self):
+#         print(f'{self.name} - {self.age}')
+        
+# person1 =Person()
+# person2 =Person()
+
+# person1.name='asf'
+# person1.age=28
+# person1.get_details()
+
+# person2.set_details('henry',15)
+# person2.get_details()
+
+# special method
+
+
+class Person :
+    
+    company ='clarusway'
+    
+    def __init__(self,name,age,gender='female'):
+        self.name=name
+        self.age =age
+        self.gender =gender
+    
+    # def set_details(self,name,age):
+    #     self.name=name
+    #     self.age=age
+        
+    def get_details(self):
+        print(f'{self.name} - {self.age} - {self.gender}')
+
+
+# person1 =Person()
+# person1.set_details('henry',15)
+
+person1=Person('nur',28,'sss')
+person2=Person('ass',28)
+person1.get_details()
+person2.get_details()

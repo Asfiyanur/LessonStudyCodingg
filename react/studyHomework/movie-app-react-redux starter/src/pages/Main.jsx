dffc0bf6849ba679aac4d4d481b98d6b5b1be7d4
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-19 19:04:06 +0300

    moviedetail

--- react/studyHomework/movie-app-react-redux starter/src/pages/Main.jsx
+++ react/studyHomework/movie-app-react-redux starter/src/pages/Main.jsx
@@ -1,15 +1,78 @@
+// import React, { useEffect, useState } from "react";
+// import useMovieCalls from "../hooks/useMovieCalls";
+// import MovieCard from "../components/MovieCard";
+// import { useSelector } from "react-redux";
+// import loadingIcon from "../assets/loadingIcon.svg";
+// import { toastWarningNotify } from "../helpers/ToastNotify";
+
+// const Main = () => {
+//   const { getMovies, getSearchMovie } = useMovieCalls();
+//   const { movies, loading } = useSelector((state) => state.movie);
+//   const { currentUser } = useSelector((state) => state.auth);
+
+//   const [pageNumber, setPageNumber] = useState(1);
+//   const [searchMovie, setSearchMovie] = useState("");
+
+//   useEffect(() => {
+//     getMovies(pageNumber);
+//   }, [pageNumber]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (searchMovie && currentUser) {
+//       getSearchMovie(searchMovie);
+//       setSearchMovie("");
+//     } else if (!currentUser) {
+//       toastWarningNotify("please logn to seach");
+//     } else {
+//       toastWarningNotify("Please enter a text");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div className="flex justify-center flex-wrap">
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="search"
+//             onChange={(e) => setSearchMovie(e.target.value)}
+//           />
+//           <button type="submit">search</button>
+//         </form>
+//         <div className="flex justify-center flex-wrap gap-8">
+//           {loading && <img src={loadingIcon} alt="loading" />}
+//           {movies?.results.map((movie) => (
+//             <MovieCard movie={movie} key={movie.id} />
+//           ))}
+//         </div>
+//       </div>
+
+//       <div className="pagination flex justify-center gap-4">
+//         <button onClick={() => pageNumber > 1 && setPageNumber(pageNumber - 1)}>
+//           Back
+//         </button>
+//         <p>{pageNumber}</p>
+//         <button onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Main;
+
 import React, { useEffect, useState } from "react";
-import useMovieCalls from "../hooks/useMovieCalls";
-import MovieCard from "../components/MovieCard";
 import { useSelector } from "react-redux";
+import MovieCard from "../components/MovieCard";
+import useMovieCalls from "../hooks/useMovieCalls";
 import loadingIcon from "../assets/loadingIcon.svg";
 import { toastWarningNotify } from "../helpers/ToastNotify";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-  const { getMovies, getSearchMovie } = useMovieCalls();
+  const { getMovies } = useMovieCalls();
   const { movies, loading } = useSelector((state) => state.movie);
   const { currentUser } = useSelector((state) => state.auth);
-
+  const navigate = useNavigate();
   const [pageNumber, setPageNumber] = useState(1);
   const [searchMovie, setSearchMovie] = useState("");
 
@@ -20,10 +83,9 @@ const Main = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (searchMovie && currentUser) {
-      getSearchMovie(searchMovie);
-      setSearchMovie("");
+      navigate("/search", { state: searchMovie });
     } else if (!currentUser) {
-      toastWarningNotify("please logn to seach");
+      toastWarningNotify("Please login to see details");
     } else {
       toastWarningNotify("Please enter a text");
     }
@@ -37,21 +99,20 @@ const Main = () => {
             type="search"
             onChange={(e) => setSearchMovie(e.target.value)}
           />
-          <button type="submit">search</button>
+          <button type="submit">Search</button>
         </form>
-        <div className="flex justify-center flex-wrap gap-8">
-          {loading && <img src={loadingIcon} alt="loading" />}
+        <div className="flex justify-center flex-wrap gap-8 my-12">
+          {loading && <img src={loadingIcon} alt="loadingImg" />}
           {movies?.results.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
       </div>
-
-      <div className="pagination flex justify-center gap-4">
+      <div className="pagination flex justify-center gap-4 my-24">
         <button onClick={() => pageNumber > 1 && setPageNumber(pageNumber - 1)}>
           Back
         </button>
-        <p>{pageNumber}</p>
+        <p className="text-2xl">{pageNumber}</p>
         <button onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
       </div>
     </div>
--- react/studyHomework/movie-app-react-redux starter/src/router/AppRouter.jsx
+++ react/studyHomework/movie-app-react-redux starter/src/router/AppRouter.jsx
@@ -1,10 +1,42 @@
+// import React, { useEffect } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Navbar from "../components/Navbar";
+// import useAuthCalls from "../hooks/useAuthCalls";
+// import Login from "../pages/Login";
+// import Main from "../pages/Main";
+// import Register from "../pages/Register";
+
+// const AppRouter = () => {
+//   const { userObserver } = useAuthCalls();
+//   useEffect(() => {
+//     userObserver();
+//   }, []);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Main />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="*" element={<Main />} />
+//       </Routes>
+//     </>
+//   );
+// };
+
+// export default AppRouter;
+
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import useAuthCalls from "../hooks/useAuthCalls";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
 import Register from "../pages/Register";
+import Search from "../pages/Search";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   const { userObserver } = useAuthCalls();
@@ -16,10 +48,14 @@ const AppRouter = () => {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" index element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
         <Route path="*" element={<Main />} />
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />} />
+        </Route>
       </Routes>
     </>
   );

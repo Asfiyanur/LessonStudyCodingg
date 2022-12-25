fb1f51d32cc85a9fd9991bc068fb8c90d5dc34a6
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-25 12:48:25 +0300

    team3-middle

--- teamwork/main/settings.py
+++ teamwork/main/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # myapp
-    
+    'task',
     
     # tridpart apps
-    'rest_framework'
+    'rest_framework',
+    'Pillow',
     
 
 ]
@@ -125,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

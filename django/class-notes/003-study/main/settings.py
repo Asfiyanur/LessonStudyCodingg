ddf2aef104f342ea4bc836f9c93e9316acdea130
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-26 16:32:49 +0300

    staudy3

--- django/class-notes/003-study/main/settings.py
+++ django/class-notes/003-study/main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # myapps
-    'fscohort'
+    'fscohort',
+    'relation',
     
     # thirtpart app
     
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL ='media/'
+
+MEDIA_ROOT = BASE_DIR / 'media/ '
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

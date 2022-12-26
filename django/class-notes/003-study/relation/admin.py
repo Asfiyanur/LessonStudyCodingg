fe218417d6153e582a8c3cd6feb095b4986b1bda
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-26 16:46:12 +0300

    relations

--- django/class-notes/003-study/relation/admin.py
+++ django/class-notes/003-study/relation/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import (
+    Profile,Address,Product
+)
+
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(Product)

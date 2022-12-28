d11d498cb70f57575ae0b71d1ab2bd186f120123
Asfiyanur <nurasfiya.13@gmail.com>
2022-12-28 13:09:06 +0300

    admin

--- django/projects/groupStudy/tutorial/views.py
+++ django/projects/groupStudy/tutorial/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Tutorial
+from .serializers import TutorialSerializer
+from rest_framework.viewsets import ModelViewSet
+
 
 # Create your views here.
+
+
+class Tutorials(ModelViewSet):
+    
+    queryset = Tutorial.objects.all()
+    serializer_class = TutorialSerializer

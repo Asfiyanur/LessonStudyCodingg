96ce04d14383a716430304566dcf556438a1f9a8
Asfiyanur <nurasfiya.13@gmail.com>
2023-01-05 20:36:37 +0300

    Blog

--- django/class-notes/010_BlogApp/blog/serializers.py
+++ django/class-notes/010_BlogApp/blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category,Blog
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -10,3 +10,27 @@ class CategorySerializer(serializers.ModelSerializer):
             'id',
             'name'
         )
+        
+        
+        
+class BlogSerializer(serializers.ModelSerializer):
+    
+    category = serializers.StringRelatedField()
+    created_id = serializers.IntegerField(write_only=True)
+    class Meta :
+        model = Blog
+        fields = (
+            'id',
+            'title',
+            'content',
+            'is_published',
+            'created_date',
+            'category',
+            'category_id'
+        )
+        
+        
+        
+        
+
+    

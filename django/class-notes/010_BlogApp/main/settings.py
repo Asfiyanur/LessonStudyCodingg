5a35e06f95c73706d5b6c12e8418f0531b3bd7d7
Asfiyanur <nurasfiya.13@gmail.com>
2023-01-06 00:33:04 +0300

    authPermis

--- django/class-notes/010_BlogApp/main/settings.py
+++ django/class-notes/010_BlogApp/main/settings.py
@@ -40,8 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'django_filters',
+    
     'blog',
-    'django_filters'
+    'user',
+    
+    'rest_framework.authtoken'
+    
 ]
 
 MIDDLEWARE = [
@@ -132,5 +137,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',
-                                'rest_framework.filters.SearchFilter']
+                                'rest_framework.filters.SearchFilter'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
